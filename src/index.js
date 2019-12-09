0a7465219e0f65654ec2b94c610ca321be1ab569
David Diaz <diazdavid.info@gmail.com>
2019-12-09 07:55:14 +0100

    primer paso

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('HOLA');
+/** @jsx h */
+
+function h(type, props, ...children) {
+    console.log(type);
+    console.log(props);
+    console.log(children);
+    return {type, props, children};
+}
+
+function createElement(node) {
+    let element = document.createElement(node.type);
+    element.appendChild(
+        document.createTextNode(node.children[0])
+    );
+    return element;
+}
+
+const box = (<h2>Hello! David</h2>);
+const box3 = (<h3>Otro h3</h3>);
+
+const $root = document.getElementById('root');
+$root.appendChild(createElement(box));
+$root.appendChild(createElement(box3));
