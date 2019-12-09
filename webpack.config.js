cd651d56f692198509e33d749197a244e1590be1
David Diaz <diazdavid.info@gmail.com>
2019-12-09 07:16:14 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public/js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    }
+};
